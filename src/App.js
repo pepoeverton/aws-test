bd753ccf3af6ba38fe3635069c5933c3b8fab4d1
pepoeverton <everton.code@gmail.com>
2021-07-01 23:08:07 -0300

    add some change v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Web previews test
+          Web previews test v2
         </a>
       </header>
     </div>
